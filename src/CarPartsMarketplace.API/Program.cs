73c82d0ac80d25e4a0874c6af7ac3526e31da7c8
ouzdev <oguzcangencc@hotmail.com>
2022-08-14 23:31:29 +0300

    ✨ Implement autofac

--- src/CarPartsMarketplace.API/Program.cs
+++ src/CarPartsMarketplace.API/Program.cs
@@ -1,7 +1,20 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using CarPartsMarketplace.Core.DependencyResolvers;
+using CarPartsMarketplace.Core.Extensions;
+using CarPartsMarketplace.Data.Context.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+var coreModule = new CoreModule();
+builder.Services.AddDependencyResolvers(builder.Configuration, new ICoreModule[] { coreModule });
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/CarPartsMarketplace.Core/Extensions/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarPartsMarketplace.Core.DependencyResolvers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarPartsMarketplace.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDependencyResolvers(this IServiceCollection services, IConfiguration configuration, ICoreModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                module.Load(services, configuration);
+            }
+        }
+    }
+}
